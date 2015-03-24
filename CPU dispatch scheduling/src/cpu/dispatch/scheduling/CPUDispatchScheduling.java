88d588342ceee61542df2f9d9124d380ccbf320a
timothy <tbradford16@my.whitworth.edu>
2015-03-24 10:51:41 -0700

    General class layout
    General class layout based on UML

---
+++ CPU dispatch scheduling/src/cpu/dispatch/scheduling/CPUDispatchScheduling.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpu.dispatch.scheduling;
+
+/**
+ *
+ * @author tbrad_000
+ */
+public class CPUDispatchScheduling {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ CPU dispatch scheduling/src/cpu/dispatch/scheduling/RoundRobin.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpu.dispatch.scheduling;
+
+/**
+ *
+ * @author tbrad_000
+ */
+public class RoundRobin {
+    
+}
